a47e429e43fbd1068e9f9417cd889448358bb51a
bgbester <90190484+bgbester@users.noreply.github.com>
2022-09-26 21:21:15 +0200

    Start

---
+++ ConnectedOffice/DeviceManagement_WebApp/Controllers/CategoriesRepository.cs
@@ -0,0 +1,19 @@
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DeviceManagement_WebApp.Repo_Settings
+{
+    public class CategoriesRepository
+    {
+        private readonly ConnectedOfficeContext _context = new ConnectedOfficeContext();
+
+        //GET: Products
+
+        public List<Category> Getall()
+        {
+            return _context.Category.ToList();
+        }
+    }
+}
